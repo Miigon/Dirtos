acc46c72055f631e9ac3006e05cfbb0552f0fc7c
Miigon <miigon.pg@gmail.com>
2016-11-12 13:08:13 +0800

    First commit

---
+++ client/dirt.js
@@ -0,0 +1,20 @@
+function DirtMud(document)
+{
+    this.doc = document;
+    document.write("DirtMud Initialized!")
+}
+
+/*
+  DirtMud Data Transport Format Tags
+  @@
+    Write them as '@'
+  @link[target] @!link 
+    !!!TODO!!!
+    
+    
+*/
+
+DirtMud.prototype.write = function(data)
+{
+    
+}
